c471a8d12dcabe1cf6f0273d66fd9b2f181a49fa
Ryan James McGill <ryanjamesmcgill@gmail.com>
2020-12-06 11:35:42 -0800

    puppeteer added

--- test.js
+++ test.js
@@ -1,32 +1,18 @@
-var request = require("request");
-var cheerio = require("cheerio");
 const puppeteer = require('puppeteer');
 
-
-var main = (cb) => {
-  loadPage();
-}
-
-
-var loadPage = async (cb) => {
+(async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto('http://www.meteocentrale.ch/it/europa/svizzera/meteo-corvatsch/details/S067910/')
+
   const result = await page.evaluate(() => {
     let temperature = document.querySelector('.column-4').innerText
     return {
       temperature
     }
   })
+
   console.log(result)
+
   browser.close()
-  return cb();
-}
-
-
-main((err, res) => {
-  if (err) {
-    throw err;
-  }
-  console.log("Done!");
-})
+})()
