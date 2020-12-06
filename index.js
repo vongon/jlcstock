269288f21336d9709a7e15196d5ec9d3fe2d855e
Ryan James McGill <ryanjamesmcgill@gmail.com>
2020-12-06 13:05:03 -0800

    querying stock

--- index.js
+++ index.js
@@ -1,32 +1,67 @@
-var request = require("request");
-var cheerio = require("cheerio");
 const puppeteer = require('puppeteer');
+const async = require('async');
 
+const parts = [
+  { partNumber: 'C7171', description: 'example capacitor' },
+  { partNumber: 'C69901', description: 'TPA6132A2RTER, headphone amp' },
+]
 
 var main = (cb) => {
-  loadPage();
-}
-
-
-var loadPage = async (cb) => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('http://www.meteocentrale.ch/it/europa/svizzera/meteo-corvatsch/details/S067910/')
-  const result = await page.evaluate(() => {
-    let temperature = document.querySelector('.column-4').innerText
-    return {
-      temperature
+  results = [];
+  async.eachSeries(parts, (part, cb) => {
+    console.log('Querying part', part.partNumber);
+    queryStock(part.partNumber, (err, result) => {
+      if (err) {
+        return cb(err);
+      }
+      part.quantity = result.quantity;
+      results.push(part);
+      return cb();
+    })
+  }, (err) => {
+    if (err) {
+      return cb(err);
     }
-  })
-  console.log(result)
-  browser.close()
-  return cb();
+    allInStock = true
+    for (item of results) {
+      if(item.quantity === '0') {
+        allInStock = false;
+      }
+    }
+
+    if(allInStock) {
+      console.log('Everything is in stock!');
+    } else {
+      console.log('At least one part out of stock : (');
+    }
+    console.log(results);
+    return cb();
+  });
+}
+
+var queryStock = (partNumber, cb) => {
+  (async () => {
+    const browser = await puppeteer.launch({headless: true})
+    const page = await browser.newPage()
+    await page.goto(`https://jlcpcb.com/parts/componentSearch?searchTxt=${partNumber}`,{waitUntil: 'networkidle2'})
+
+    const result = await page.evaluate(() => {
+      let quantity = document.querySelector('td.ng-binding').innerText;
+      return {
+        quantity
+      }
+    })
+    browser.close()
+    result.partNumber = partNumber;
+    return cb(null, result)
+  })()
 }
 
 
-main((err, res) => {
+main((err, result) => {
   if (err) {
     throw err;
   }
   console.log("Done!");
 })
+
